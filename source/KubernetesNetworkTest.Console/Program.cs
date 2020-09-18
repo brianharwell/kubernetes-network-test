b37c47105839e94f5d7e783cb3b5018585529c83
Brian Harwell <bharwell@paylocity.com>
2020-09-18 08:52:15 -0500

    Add console app

---
+++ source/KubernetesNetworkTest.Console/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace KubernetesNetworkTest.Console
+{
+  public class Program
+  {
+    public static async Task Main(string[] args)
+    {
+      if (args.Length != 1)
+      {
+        System.Console.WriteLine("ERROR: Destination url must be specified as the only argument");
+
+        Environment.ExitCode = 1;
+
+        return;
+      }
+
+      var url = args[0];
+
+      System.Console.WriteLine($"Connecting to '{url}'");
+
+      try
+      {
+        var httpClient = new HttpClient();
+        var httpResponseMessage = await httpClient.GetAsync(url);
+
+        if (!httpResponseMessage.IsSuccessStatusCode)
+        {
+          System.Console.WriteLine($"ERROR: Connection failed: {(int)httpResponseMessage.StatusCode} ({httpResponseMessage.StatusCode})");
+
+          Environment.ExitCode = 1;
+
+          return;
+        }
+
+        System.Console.WriteLine("Connection succeeded");
+      }
+      catch (Exception exception)
+      {
+        System.Console.WriteLine(exception);
+        throw;
+      }
+    }
+  }
+}
--- source/KubernetesNetworkTest.Web/Program.cs
+++ source/KubernetesNetworkTest.Web/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-namespace KubernetesNetworkTest
+namespace KubernetesNetworkTest.Web
 {
   public class Program
   {
